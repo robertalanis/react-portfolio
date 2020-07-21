884c0e74fe249622ca78baaa4df5d404df4dff0f
robertalanis <rxalanis@gmail.com>
2020-07-21 17:07:52 -0600

    Updates

---
+++ precache-manifest.e1ac4d4c4de6644854250cf5047da712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac17117634abea5e4c6efb863fb47d6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "57eadf9da9a9cbae6e44",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9eff8dc14fec2ffe414",
+    "url": "/react-portfolio/static/css/main.1eee8d51.chunk.css"
+  },
+  {
+    "revision": "57eadf9da9a9cbae6e44",
+    "url": "/react-portfolio/static/js/2.a251adc5.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react-portfolio/static/js/2.a251adc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9eff8dc14fec2ffe414",
+    "url": "/react-portfolio/static/js/main.16056409.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
